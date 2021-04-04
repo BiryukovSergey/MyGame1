fe23957658ae626fea52aa94c4c8bbe7a48c01cd
BiryukovSergey <twc2012@yandex.ru>
2021-04-04 11:07:52 +0300

    Подправил анимацию с прыжком и сделал завершение уровня!

---
+++ Assets/Finish.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField] private GameObject _key;
+    [SerializeField] private TMP_Text _text;
+
+    private void Start()
+    {
+        _text.gameObject.SetActive(false);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player")&&_key == null)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                _text.gameObject.SetActive(true);
+            }
+        }
+    }
+}
